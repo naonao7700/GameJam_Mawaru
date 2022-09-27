02a9ee490c5be1550b8281759c4bf605cd33680c
naonao7700_Home <takiyama.vantan@gmail.com>
2022-09-28 08:21:35 +0900

    プレイヤーテスト更新

--- Assets/GameManager/Scripts/Sample/PlayerObject.cs
+++ Assets/GameManager/Scripts/Sample/PlayerObject.cs
@@ -15,8 +15,22 @@ public class PlayerObject : MonoBehaviour
 	[Range(0,360)] public float mainRot;
 	[Range(0,360)] public float subRot;
 
+	[SerializeField] private GameObject prefab;
+
+	private void TestUpdate()
+	{
+		mainRot += Input.GetAxis("Horizontal");
+		mainRot %= 360.0f;
+		if( Input.GetKeyDown(KeyCode.Space) )
+		{
+			GameObject.Instantiate(prefab, mainShotPos.position, mainShot.localRotation);
+		}
+	}
+
 	private void Update()
 	{
+		TestUpdate();
+
 		mainShot.localEulerAngles = new Vector3(0, 0, mainRot);
 		subShot.localEulerAngles = new Vector3(0, 0, subRot);
 		character.angle = mainRot;
